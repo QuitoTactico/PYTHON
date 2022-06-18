3b47460fcb419eea6a994b775f768c08b547611b
Esteban Vergara Giraldo <Teteban0917@gmail.com>
2022-06-18 14:37:38 -0500

    Calendario

--- al_9.py
+++ al_9.py
@@ -1,5 +1,6 @@
 #Mi favorito hasta ahora
 from datetime import datetime
+mes = {1:"Enero",2:"Febrero",3:"Marzo",4:'Abril',5:'Mayo',6:'Junio',7:'Julio',8:'Agosto',9:'Septiembre',10:'Octubre',11:'Noviembre',12:'Diciembre'}
 def calendario(y, m):
     d , s = {1:31,2:28,3:31,4:30,5:31,6:30,7:31,8:31,9:30,10:31,11:30,12:31} , datetime.strptime(f'1/{m}/{y}','%d/%m/%Y').weekday()
     d[2] = 29  if y%4 == 0 and (y%100 != 0 or y%400==0)else 28
@@ -10,7 +11,15 @@ def calendario(y, m):
         else: print(i,end="")
         s += 1
         if s == 7:print();s=0
-for _ in range(int(input())):
-    s = list(map(int,input().split("/")))
-    calendario(s[2],s[1])
-    print()
+modo = int(input("Ingrese -1 para obtener el calendario de todo el año. -> "))
+if modo == -1:
+    a = int(input("Qué año?\n"))
+    for i in range(1,13):
+        print("___________________________________________________ "+mes[i])
+        calendario(a,i)
+        print()
+else:
+    for _ in range(modo):
+        s = list(map(int,input().split("/")))
+        calendario(s[2],s[1])
+        print("\n")
