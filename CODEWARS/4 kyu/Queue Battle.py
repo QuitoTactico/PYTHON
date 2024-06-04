f07bdd1e24716a357ebc1b054d1546583e846398
QuitoTactico <teteban0917@gmail.com>
2024-06-04 14:49:04 -0500

    Finished my first 4 kyu! (It was so funny)

--- CODEWARS/4 kyu/Queue Battle.py
+++ CODEWARS/4 kyu/Queue Battle.py
@@ -17,9 +17,9 @@ class Bullet:
 		self.dist = dist
 		self.target = target
 	def __str__(self):
-		return f'{self.dist}|{self.target}'
+		return f'{self.dist}.{self.vel}|{self.target}'
 	def __repr__(self):
-		return f'{self.dist}|{self.target}'
+		return f'{self.dist}.{self.vel}|{self.target}'
 
 def armies_creation(armies) -> list[list[Soldier]]:
 	new_armies = []
@@ -38,9 +38,9 @@ def queue_battle(dist:int,*armies):
 
 
 	while len(armies) > 1:
-		#print()
 		#print(armies)
 		#print(bullets)
+		#print()
 
 		# the bullets hit while the soldiers prepare
 		army_died = False
@@ -52,7 +52,7 @@ def queue_battle(dist:int,*armies):
 				bullet.dist -= bullet.vel 					# flying bullet
 				target_index = bullet.target
 
-				if bullet.dist <= 0 and target_index not in dead_armies: 						# the bullet hits
+				if bullet.dist <= 0 and target_index not in dead_armies and target_index not in dead_heads: 						# the bullet hits
 					#print(bullets, len(bullets))
 					#bullets.pop(bullet_index)
 					dead_heads.append(target_index)
@@ -65,7 +65,8 @@ def queue_battle(dist:int,*armies):
 						army_died = True
 						#break
 				else:
-					new_bullets.append(bullet)
+					if bullet.dist > 0 or target_index not in dead_heads:
+						new_bullets.append(bullet)
 
 		bullets = new_bullets
 
@@ -73,6 +74,7 @@ def queue_battle(dist:int,*armies):
 			new_armies = []
 			for army_index, army in enumerate(armies):
 				if army_index not in dead_armies:
+					army.append(army.pop(0))
 					new_armies.append(army)
 			armies = new_armies
 			bullets = []
@@ -89,7 +91,7 @@ def queue_battle(dist:int,*armies):
 				#else:
 					#print(army_index)
 	
-	if len(armies) == 1:
+	if len(armies) == 1 and len(armies[0]) != 0:
 		army_index = armies[0][0].army
 		winners = tuple([soldier.pos for soldier in armies[0]])
 		return army_index, winners
@@ -98,22 +100,29 @@ def queue_battle(dist:int,*armies):
 		return -1, ()
 
 
+print(queue_battle(100,
+				   (25,38,55,46,82),
+				   (64,90,37,25,58))) 				# (1,(3,2))
 
-			
-	
-	
-    
+print(queue_battle(200,
+				   (61,83,37,55,92,35,68,72),
+				   (90,81,36,114,67,25,31,84)))  	# (0,(6,7))
 
-#print(queue_battle(500,(345,168,122,269,151),(56,189,404,129,101),(364,129,209,163,379),(520,224,154,74,420)))
-#print(queue_battle(100,(25,38,55,46,82),(64,90,37,25,58)))
-# (2,(0,2,5))
+print(queue_battle(300,
+				   (98,112,121,95,63),
+				   (120,94,90,88,30),
+				   (116,144,45,200,32))) 			# (0,(2,))
 
-A = (98,112,121,95,63)
-B = (120,94,90,88,30)
-C = (116,144,45,200,32)
-#print(queue_battle(300,A,B,C)) #(0,(2,))
+print(queue_battle(400,
+				   (186,78,56,67,78,127,78,192),
+				   (78,67,208,45,134,212,82,99),
+				   (327,160,49,246,109,98,44,57))) 	# (2,(0,2,5))
 
-print(queue_battle(100,(25,38,55,46,82),(64,90,37,25,58))) # (1,(3,2))
+print(queue_battle(500,
+				   (345,168,122,269,151),
+				   (56,189,404,129,101),
+				   (364,129,209,163,379),
+				   (520,224,154,74,420))) 			# (-1,())
 
 #a = [1,2,3]
 #a.append(a.pop(0))
