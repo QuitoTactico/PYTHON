db1be5c75e99c292f0932b287094aa10b86cc293
QuitoTactico <teteban0917@gmail.com>
2024-06-04 15:56:23 -0500

    before another 4 kyu

---
+++ Trucazos/255_validation.py
@@ -0,0 +1,14 @@
+# This is a javascript solution, maybe there's something similar on python
+
+'''
+^\d$            : Matches any single digit from 0 to 9.
+
+^[1-9]\d$       : Matches two-digit numbers from 10 to 99.
+
+^1\d\d$         : Matches three-digit numbers from 100 to 199.
+
+^2[0-4]\d$      : Matches numbers from 200 to 249.
+
+^25[0-5]$       : Matches numbers from 250 to 255.
+
+'''
