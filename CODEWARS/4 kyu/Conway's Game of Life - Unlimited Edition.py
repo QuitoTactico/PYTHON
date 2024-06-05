e3cbe9b2bbab9d3b0a9ba3e8873cfb4f22110f18
QuitoTactico <teteban0917@gmail.com>
2024-06-05 00:42:26 -0500

    Conway

--- CODEWARS/4 kyu/Conway's Game of Life - Unlimited Edition.py
+++ CODEWARS/4 kyu/Conway's Game of Life - Unlimited Edition.py
@@ -1,10 +1,32 @@
-#from preloaded import htmlize # this can help you debug your code
-
-# Any live cell with fewer than two live neighbours dies, as if caused by underpopulation.
-# Any live cell with more than three live neighbours dies, as if by overcrowding.
-# Any live cell with two or three live neighbours lives on to the next generation.
-# Any dead cell with exactly three live neighbours becomes a live cell.
 import copy
+
+def conway_printer(cells):
+    for i in cells:
+        for j in i:
+            if j == 1:
+                print("▓▓", end='')
+            else:
+                print("░░", end='')
+        print()
+
+def expand(cells):
+    cells = [[0 for _ in range(len(cells[0]))]] + cells + [[0 for _ in range(len(cells[0]))]]
+    cells = [[0]+item+[0] for item in cells]
+    return cells
+
+def reduce(cells):
+    for _ in range(max(len(cells), len(cells[0]))):
+        if sum(cells[0]) == 0: cells = cells[1:]
+        if sum(cells[-1]) == 0: cells = cells[:-1]
+        
+        left, right = 0, 0
+        for i in cells:
+            left += i[0]
+            right += i[-1]
+
+        if left == 0: cells = [item[1:] for item in cells]
+        if right == 0: cells = [item[:-1] for item in cells]
+    return cells
 
 def get_neighbours(cells : list[list[int]], x, y) -> int:
     count = 0
@@ -19,6 +41,9 @@ def get_generation(cells : list[list[int]], generations : int) -> list[list[int]
     
     for _ in range(generations):
         past_game = copy.deepcopy(game)
+        past_game = expand(past_game)
+
+        game = copy.deepcopy(past_game)
 
         #conway_printer(past_game)
 
@@ -34,7 +59,11 @@ def get_generation(cells : list[list[int]], generations : int) -> list[list[int]
                     if neighbours == 3:
                         game[i][j] = 1
 
+        game = reduce(game)
+        
     return game
+
+
 
 
 '''
@@ -58,17 +87,36 @@ cells = [
     [1,1,0]
 ]
 
-
-def conway_printer(conway):
-    for i in conway:
-        for j in i:
-            if j == 1:
-                print("▓▓", end='')
-            else:
-                print("░░", end='')
-        print()
-
-
 conway_printer(get_generation(cells, 1))
-#conway_printer(cells)
-#print(htmlize(cells))
+
+'''
+cells = [[0 for _ in range(len(cells[0]))]] + cells + [[0 for _ in range(len(cells[0]))]]
+cells = [[0]+item+[0] for item in cells]
+
+conway_printer(cells)
+'''
+'''
+for i in range(3):
+    cells = expand(cells)
+conway_printer(cells)
+
+cells = reduce(cells)
+conway_printer(cells)
+'''
+
+
+# -------------------------------------- HAHHAHAHA WTF IS THIS-----------------------------
+
+def get_neighbours2(x, y):
+    return {(x + i, y + j) for i in range(-1, 2) for j in range(-1, 2)}
+
+def get_generation2(cells, generations):
+    if not cells: return cells
+    xm, ym, xM, yM = 0, 0, len(cells[0]) - 1, len(cells) - 1
+    cells = {(x, y) for y, l in enumerate(cells) for x, c in enumerate(l) if c}
+    for _ in range(generations):
+        cells = {(x, y) for x in range(xm - 1, xM + 2) for y in range(ym - 1, yM + 2)
+                    if 2 < len(cells & get_neighbours(x, y)) < 4 + ((x, y) in cells)}
+        xm, ym = min(x for x, y in cells), min(y for x, y in cells)
+        xM, yM = max(x for x, y in cells), max(y for x, y in cells)
+    return [[int((x, y) in cells) for x in range(xm, xM + 1)] for y in range(ym, yM + 1)]
