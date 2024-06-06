8652df06ccfdd1f37b09cf0bb8391be6b8056e7e
QuitoTactico <teteban0917@gmail.com>
2024-06-06 11:49:22 -0500

    ni idea papu

--- tabla.py
+++ tabla.py
@@ -169,10 +169,14 @@ lista3 = [
     ,548
 ]
 
+lista4 = list(map(int, '100, 100, 101, 101, 101, 102, 102, 102, 103, 103, 103, 104, 104, 107, 109, 110, 110, 110, 111, 111, 113, 113, 115, 115, 116, 116, 116, 118, 118, 120, 123, 123, 124, 125, 125, 125, 126, 126, 127, 127, 127, 127, 128, 130, 130, 130, 130, 131, 132, 133, 134, 137, 137, 137, 138, 139, 140, 140, 141, 141, 142, 143, 144, 145, 146, 146, 146, 147, 148, 148, 149, 150, 151, 152, 153, 153, 153, 153, 153, 154, 154, 155, 157, 158, 158, 158, 158, 160, 160, 160, 161, 162, 162, 163, 163, 164, 165, 165, 166, 167, 167, 168, 168, 168, 168, 169, 169, 171, 171, 171, 171, 171, 172, 174, 175, 175, 175, 176, 176, 177, 177, 177, 177, 178, 178, 179, 179, 180, 180, 181, 181, 182, 182, 184, 184, 185, 185, 186, 186, 186, 187, 187, 187, 188, 189, 189, 190, 190, 191, 191, 191, 195, 196, 196, 197, 198, 198, 199, 199, 199, 200, 200'.split(', ')))
+
+print(lista4)
+
 
 #----------------------------------------------
 
-datos = lista3
+datos = lista4
 
 #----------------------------------------------
 
@@ -193,13 +197,37 @@ tablafrec = {}
 
 i = np.min(datos)
 num = 1
-print('i','Li','Ls','MC','F','Fa','Fr','F%','Fra','Fra%', sep='\t')
+prom = sum(datos) / len(datos)
+
+print('i',
+      'Li',
+      'Ls',
+      'MC',
+      'F',
+      'Fa',
+      'Fr',
+      'F%',
+      'Fra',
+      'Fra%',
+
+      '|',
+
+      'Fr*MC', 
+      '(MC-X)*F', 
+      '(MC^2)*F', 
+      'MC*F', 
+      '(MC*F)^2',
+
+      sep='\t')
 print('-'*80)
+
 while i < np.max(datos):
     inferior = i
     superior = i + amplitud - 1
     marca_clase = (inferior + superior) / 2
-    frecuencia = len([x for x in datos if x >= inferior and x <= superior])
+    datos_clase = [x for x in datos if x >= inferior and x <= superior]
+    frecuencia = len(datos_clase)
+    prom_clase = sum(datos_clase) / frecuencia
     if num == 1:
         frec_acum = frecuencia
     else:
@@ -212,7 +240,33 @@ while i < np.max(datos):
         frec_rel_acum = frec_rel_acum+frecuencia_relativa
     frec_rel_acum_porcentual = frec_rel_acum * 100
 
-    print(num, inferior, superior, marca_clase, frecuencia, frec_acum, round(frecuencia_relativa,2), str(round(frecuencia_porcentual,2))+'%', round(frec_rel_acum,2), str(round(frec_rel_acum_porcentual,2))+'%', sep='\t')
+    print(num, 
+          inferior, 
+          superior, 
+          marca_clase, 
+          frecuencia, 
+          frec_acum, 
+          round(frecuencia_relativa,2), 
+          str(round(frecuencia_porcentual,2))+'%', 
+          round(frec_rel_acum,2), 
+          str(round(frec_rel_acum_porcentual,2))+'%', 
+
+          '|',
+
+          round(frecuencia_relativa*marca_clase,2), 
+          round((marca_clase-prom_clase)*frecuencia,2), 
+          round((marca_clase**2)*frecuencia,2), 
+          round(marca_clase*frecuencia,2), 
+          round((marca_clase*frecuencia)**2,2), 
+          
+          sep='\t')
+    
+    #'Fr*X*MC', 
+    #'(MC-X)Fa', 
+    #'MC^2*X*Fa', 
+    #'MC*Fa', 
+    #'(MC Fa)^2',
+    
     i += amplitud
     num += 1
 
