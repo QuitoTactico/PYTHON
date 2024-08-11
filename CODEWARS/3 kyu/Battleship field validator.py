1e53ad44b0fc1b62f1d939f60e07e404401ef4c4
QuitoTactico <teteban0917@gmail.com>
2024-08-10 21:12:15 -0500

    To study later

--- CODEWARS/3 kyu/Battleship field validator.py
+++ CODEWARS/3 kyu/Battleship field validator.py
@@ -72,3 +72,55 @@ battleField = [[1, 0, 0, 0, 0, 1, 1, 0, 0, 0],
 
 
 print(validate_battlefield(battleField))
+
+
+# ----------------- WTFFFFFF -------------------------
+
+'''
+from scipy.ndimage.measurements import label, find_objects, np
+def validate_battlefield(field):
+    field = np.array(field)
+    return sorted(
+        ship.size if min(ship.shape) == 1 else 0
+        for ship in (field[pos] for pos in find_objects(label(field, np.ones((3,3)))[0]))
+    ) == [1,1,1,1,2,2,2,3,3,4]
+
+
+    
+from scipy.ndimage.measurements import label, find_objects
+import numpy as np
+def validate_battlefield(field):
+    field = np.array(field)
+    return sorted(
+        ship.size if min(ship.shape) == 1 else 0
+        for ship in (field[pos] for pos in find_objects(label(field, np.ones((3,3)))[0]))
+    ) == [1,1,1,1,2,2,2,3,3,4]
+
+
+
+def validate_battlefield(field):
+    for i in range(10):
+        for j in range(10):
+            if field[i][j]:
+                field[i][j] = sum(1 for x in range(max(i - 1, 0), min(i + 1, 9) + 1)
+                                  for y in range(max(j - 1, 0), min(j + 1, 9) + 1) if field[x][y])
+    return sum(sum(row) for row in field) == 40
+
+    
+
+import re
+def validate_battlefield(field):
+    def check(ship):
+        for i in range(len(matcher)):
+            ship["count"] = ship.get("count", 0) + (1 if ship["regex"].match(matcher, i) else 0)
+        return ship["count"] // ship["factor"] == ship["amount"]
+    ships = [
+        { "name": "battleship","amount": 1,"factor": 1,"regex": re.compile("[^1]{6}.{5}[^1]1111[^1].{5}[^1]{6}")},
+        { "name": "cruisers",  "amount": 2,"factor": 1,"regex": re.compile("[^1]{5}.{6}[^1]111[^1].{6}")},
+        { "name": "destroyers","amount": 3,"factor": 1,"regex": re.compile("[^1]{4}.{7}[^1]11[^1].{7}[^1]{4}")},
+        { "name": "submarines","amount": 4,"factor": 2,"regex": re.compile("[^1]{3}.{8}[^1]1[^1].{8}[^1]{3}")},
+    ]
+    joinx = lambda f: "0"*12 + ",".join("".join(map(str, l)) for l in f) + "0"*12
+    matcher = joinx(field + [[0]*10] + list(zip(*field)))
+    return all(map(check, ships))
+'''
