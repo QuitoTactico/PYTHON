23e3c1ec69ecc7b5f87291e01c970792b56fdfc9
QuitoTactico <teteban0917@gmail.com>
2024-08-10 23:13:20 -0500

    I've learnt something new

---
+++ Trucazos/Retornar_funciones.py
@@ -0,0 +1,11 @@
+def funcion_principal():
+    def funcion_anidada():
+        print("¡Hola desde la función anidada!")
+
+    return funcion_anidada
+
+# Llamamos a la función principal para obtener la función anidada
+mi_funcion = funcion_principal()
+
+# Llamamos a la función anidada retornada
+mi_funcion()
