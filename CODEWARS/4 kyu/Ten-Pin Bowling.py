a25a8ddf4b21f91c1aee7a553fc51bc10a17fbf4
QuitoTactico <teteban0917@gmail.com>
2024-08-11 14:48:53 -0500

    i'll finish this someday

---
+++ CODEWARS/4 kyu/Ten-Pin Bowling.py
@@ -0,0 +1,22 @@
+# https://www.bowlinggenius.com/
+
+def bowling_score(frames:str):
+    frames_list = frames.split()
+
+    total = 0
+    bonus1 = False
+    bonus2 = False
+    double_bonus = False
+
+    for index, frame in enumerate(frames_list):
+
+        if frame.isnumeric(): # 62
+            total += sum(int(j) for j in frame)
+            
+        elif index == 'X':
+            total += 10
+        elif index == 'XXX':
+            total += 30
+
+
+bowling_score('11 11 11 11 11 11 11 11 11 11')
