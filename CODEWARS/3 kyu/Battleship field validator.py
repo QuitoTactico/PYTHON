6f0324f2e0f42cfa6d46343d0110f3251f57a48f
QuitoTactico <teteban0917@gmail.com>
2024-08-10 21:04:35 -0500

    XD, finally

--- CODEWARS/3 kyu/Battleship field validator.py
+++ CODEWARS/3 kyu/Battleship field validator.py
@@ -11,20 +11,47 @@ def validate_battlefield(field):
         if sum([sum(row) for row in field]) != 20: return False # wrong ships count
     except:
         return False # wrong dimensions too (smaller than expected)
+    
 
-    '''
-    def count_ship(x, y):
+    horizontal_validation = [(1, i) for i in [-1,0,1]]+[(-1, 1)]
+    vertical_validation = [(i, 1) for i in [-1,0,1]]+[(1, -1)]
+    dot_validation = [(-1,1),(1,1),(1,-1)]
+
+    def count_ship(y, x):
         dot_count = 0
-        if x+1 field[y][x+1] == 1: # horizontal ship
+
+        if x+1 < 10 and field[y][x+1] == 1:                 # horizontal ship
+            while x < 10 and field[y][x] == 1:
+                field[y][x] = 0
+                dot_count += 1
+                for j, i in horizontal_validation:  # touch validation
+                    if 0 <= x+i < 10 and 0 <= y+j < 10 and field[y+j][x+i] == 1: return False
+                x += 1
+
+        elif y+1 < 10 and field[y+1][x] == 1:               # vertical ship
+            while y < 10 and field[y][x] == 1:
+                field[y][x] = 0
+                dot_count += 1
+                for j, i in vertical_validation: # touch validation
+                    if 0 <= x+i < 10 and 0 <= y+j < 10 and field[y+j][x+i] == 1: return False
+                y += 1
+
+        else:   # dot ship
+            field[y][x] = 0
+            dot_count = 1
+            for j, i in vertical_validation: # touch validation
+                    if 0 <= x+i < 10 and 0 <= y+j < 10 and field[y+j][x+i] == 1: return False
             
-        elif field[y+1][x] == 1: # vertical ship
+        counter[dot_count] -= 1
+        return True
+    
 
     for i, row in enumerate(field):
         for j, point in enumerate(row):
             if point == 1:
-                if not count_ship(): # invalid ship
+                if not count_ship(i, j): # invalid ship
                     return False
-    '''
+    
     
     return True if list(counter.values()) == [0]*4 else False # wrong ship count
         
