a5c6b4ff21c5f5cca7359aaa913016e20dfd9bc9
QuitoTactico <teteban0917@gmail.com>
2024-08-10 20:07:44 -0500

    tests fixed

--- CODEWARS/3 kyu/Battleship field validator.py
+++ CODEWARS/3 kyu/Battleship field validator.py
@@ -1,4 +1,4 @@
-#There must be single battleship (size of 4 cells), 2 cruisers (size 3), 3 destroyers (size 2) and 4 submarines (size 1). Any additional ships are not allowed, as well as missing ships.
+#There must be a single battleship (size of 4 cells), 2 cruisers (size 3), 3 destroyers (size 2) and 4 submarines (size 1). Any additional ships are not allowed, as well as missing ships.
 
 def validate_battlefield(field):
     counter = {4:1,  # battleship
@@ -6,23 +6,31 @@ def validate_battlefield(field):
                2:3,  # destroyers
                1:4}  # submarines
     
+    try:
+        if len(field) != 10 or len(field[0]) != 10: return False # wrong dimensions
+        if sum([sum(row) for row in field]) != 20: return False # wrong ships count
+    except:
+        return False # wrong dimensions too (smaller than expected)
+
+    '''
     def count_ship(x, y):
-        for 
+        dot_count = 0
+        if x+1 field[y][x+1] == 1: # horizontal ship
+            
+        elif field[y+1][x] == 1: # vertical ship
 
     for i, row in enumerate(field):
         for j, point in enumerate(row):
             if point == 1:
-                if not count_ship(field): # invalid ship
+                if not count_ship(): # invalid ship
                     return False
+    '''
     
-    return True if sum(counter.values()) == 0 else False
+    return True if list(counter.values()) == [0]*4 else False # wrong ship count
         
-    
-                
 
 
-
-
+# ------------------------------------------------------------------------------
 
 battleField = [[1, 0, 0, 0, 0, 1, 1, 0, 0, 0],
                [1, 0, 1, 0, 0, 0, 0, 0, 1, 0],
@@ -34,3 +42,6 @@ battleField = [[1, 0, 0, 0, 0, 1, 1, 0, 0, 0],
                [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
                [0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+
+
+print(validate_battlefield(battleField))
